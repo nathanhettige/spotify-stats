290eb032476c345a558bebd845971e0458253b1d
Nathan Hettige <nathan.hettige@gmail.com>
2026-03-14 16:31:17 +1000

    feat: set up tabs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Header from "@/features/header"
 import Footer from "@/features/footer"
 import { ProfileHeader } from "@/features/profile-header"
 import meQueryOptions from "@/lib/spotify/api/me/me"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export const Route = createFileRoute("/")({
   component: IndexPage,
@@ -25,13 +26,33 @@ function App() {
     <div className="grid min-h-svh grid-rows-[auto_1fr_auto]">
       <Header />
 
-      <main>
+      <main className="space-y-10 px-4">
         <ProfileHeader
           followers={user?.followers.total ?? 0}
           name={user?.display_name ?? ""}
           imageUrl={user?.images[0]?.url ?? ""}
           profileUrl={user?.external_urls.spotify ?? ""}
         />
+
+        <Tabs>
+          <TabsList className="w-full">
+            <TabsTrigger value="playlists">Playlists</TabsTrigger>
+            <TabsTrigger value="top-artists">Top Artists</TabsTrigger>
+            <TabsTrigger value="top-tracks">Top Tracks</TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="playlists">
+            <div>Stats</div>
+          </TabsContent>
+
+          <TabsContent value="top-artists">
+            <div>Top Artists</div>
+          </TabsContent>
+
+          <TabsContent value="top-tracks">
+            <div>Top Tracks</div>
+          </TabsContent>
+        </Tabs>
       </main>
 
       <Footer />
